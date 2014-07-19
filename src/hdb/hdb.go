d3626cd5e83bdb8dab19e60cc91fc7f34eee02b1
I076326 <subhobrata.dey@sap.com>
2014-07-19 15:18:06 +0530

    pushing new apis-2

--- src/hdb/hdb.go
+++ src/hdb/hdb.go
@@ -16,6 +16,7 @@ type Model struct {
 	WhereStr        string
 	ParamStr        []interface{}
 	OrderStr        string
+	OrderType		string
 	ColumnStr       string
 	PrimaryKey      string
 	JoinStr         string
@@ -46,12 +47,28 @@ func Connect(dsn string, username string, password string, onDebug bool) (conn *
 }
 
 func InitializeModel(conn *odbc.Connection, options ...interface{}) (model Model) {	
-
+	
 	if len(options) == 0 {
 		model = Model{Db: conn,ColumnStr: "*",QuoteIdentifier: "\""}
 	}else {
 		model = Model{Db: conn,ColumnStr: options[0].(string),QuoteIdentifier: options[1].(string)}
 	}
+
+	model.SchemaName = ""
+	model.TableName = ""
+	model.LimitStr = 0
+	model.OffsetStr = 0
+	model.WhereStr = ""
+	model.ParamStr = make([]interface{}, 0)
+	model.OrderStr = ""
+	model.OrderType = "ASC"
+	model.ColumnStr = "*"
+	model.PrimaryKey = "MANDT"
+	model.JoinStr = ""
+	model.GroupByStr = ""
+	model.HavingStr = ""
+	model.ParamIteration = 1
+
 	return
 }
 
@@ -69,6 +86,8 @@ func (orm *Model) SetTable(tableName string) *Model{
 
 func (orm *Model) SetPrimaryKey(primaryKey string) *Model{
 
+	//support composite primary key
+
 	fmt.Println(orm.PrimaryKey)
 	orm.PrimaryKey = primaryKey
 	fmt.Println(orm.PrimaryKey)
@@ -83,7 +102,7 @@ func (orm *Model) SetWhereClause(querystring interface{}, onDebug bool,args ...i
 					orm.WhereStr = querystring
 		case nil :
 					if orm.PrimaryKey != "" {
-						orm.WhereStr = fmt.Sprintf("%v%v%v IS NOT NULL", orm.QuoteIdentifier,orm.PrimaryKey,orm.QuoteIdentifier)
+						orm.WhereStr = fmt.Sprintf("%v IS NOT NULL", orm.PrimaryKey)
 						orm.ParamIteration++
 					}else{
 						if onDebug==true{
@@ -100,7 +119,7 @@ func (orm *Model) SetWhereClause(querystring interface{}, onDebug bool,args ...i
 func (orm *Model) GenerateSQL(onDebug bool) (sqlstmt string){
 
 	if orm.ColumnStr !="" && orm.TableName !="" && orm.SchemaName !=""{
-		sqlstmt = fmt.Sprintf("SELECT %v%v%v FROM %v%v%v.%v%v%v",orm.QuoteIdentifier,orm.ColumnStr,orm.QuoteIdentifier,orm.QuoteIdentifier,orm.SchemaName,orm.QuoteIdentifier,orm.QuoteIdentifier,orm.TableName,orm.QuoteIdentifier)
+		sqlstmt = fmt.Sprintf("SELECT %v FROM %v.%v",orm.ColumnStr,orm.SchemaName,orm.TableName)
 	}else{
 		if onDebug{
 			fmt.Println("Column String or Schema Name or Table Name is not set")
@@ -110,6 +129,77 @@ func (orm *Model) GenerateSQL(onDebug bool) (sqlstmt string){
 	if orm.WhereStr != "" {
 		sqlstmt = fmt.Sprintf("%v WHERE %v",sqlstmt,orm.WhereStr)
 	}
+	if orm.GroupByStr !="" {
+		sqlstmt = fmt.Sprintf("%v GROUP BY %v",sqlstmt,orm.GroupByStr)
+	}
+	if orm.HavingStr != "" {
+		if orm.GroupByStr != ""{
+			sqlstmt = fmt.Sprintf("%v HAVING %v",sqlstmt,orm.HavingStr)
+		}else{
+			if onDebug{
+				fmt.Println("Group By clause not set")
+			}
+			return sqlstmt
+		}
+	}
+	if orm.OrderStr !="" {
+		sqlstmt = fmt.Sprintf("%v ORDER BY %v %v",sqlstmt,orm.OrderStr,orm.OrderType)
+	}
+	if orm.OffsetStr > 0 {
+		if orm.LimitStr > 0 {
+			sqlstmt = fmt.Sprintf("%v LIMIT %v OFFSET %v",sqlstmt,orm.LimitStr,orm.OffsetStr)
+		}else{
+			if onDebug{
+				fmt.Println("Limit not specified...Offset is meaningless")
+			}
+			return sqlstmt
+		}
+	}
+	if orm.LimitStr > 0 && orm.OffsetStr == 0{
+		sqlstmt = fmt.Sprintf("%v LIMIT %v",sqlstmt,orm.LimitStr)
+	}
 	fmt.Println(sqlstmt)
 	return sqlstmt
+}
+
+func (orm *Model) SetLimit(start int,size ...int) *Model{
+
+	orm.LimitStr = start
+	if len(size) > 0 {
+		orm.OffsetStr = size[0]
+	}
+	return orm
+}
+
+func (orm *Model) SetOffset(offset int) *Model{
+
+	orm.OffsetStr = offset
+	return orm
+}
+
+func (orm *Model) SetOrderBy(order string,ordertype ...string) *Model{
+	
+	orm.OrderStr = order
+	if len(ordertype) > 0 {
+		orm.OrderType = ordertype[0]
+	}
+	return orm
+}
+
+func (orm *Model) SetColumnString(columnStr string) *Model{
+
+	orm.ColumnStr = columnStr
+	return orm
+}
+
+func (orm *Model) SetGroupBy(keys string) *Model{
+
+	orm.GroupByStr = keys
+	return orm
+}
+
+func (orm *Model) SetHaving(conditions string) *Model{
+
+	orm.HavingStr = conditions
+	return orm
 }
